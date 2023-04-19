7e68a8a8f79e001c5fdefab78f7aaf7ffc8106f0
Paul Okoli <paulokoli7@gmail.com>
2023-04-19 11:33:27 +0000

    updating 3-all.js files

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,8 +3,8 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
-    .then((resolve) => {
-      consloe.log(`${resolve[0].body} ${resolve[1].firstName} ${resolve[1].lastName}`);
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
