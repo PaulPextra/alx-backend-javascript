f632086f2197a3ae3738bf510c581deadca4bdea
Paul Okoli <paulokoli7@gmail.com>
2023-04-19 10:05:46 +0000

    updating 0-promise.js file

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,3 @@
-export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    resolve("Resolved");
-    reject("Failed");
-  });
+export default function getResponseFromAPI(){
+    return new Promise(() => {});
 }
