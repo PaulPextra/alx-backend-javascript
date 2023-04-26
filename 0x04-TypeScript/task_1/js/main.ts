9c0f68f30bcef38fbc8b6d278baed8b07b953a1c
Paul Okoli <paulokoli7@gmail.com>
2023-04-26 20:25:23 +0000

    task 3 solution

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,4 +10,14 @@ interface Teacher {
 // Solution for task 2
 interface Directors extends Teacher {
   numberOfReports: number;
+  }
+
+// Solution for task 3
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
 }
+
+const printTeacher: printTeacherFunction = (
+  firstName: string,
+  lastName: string
+) => `${firstName.substring(0, 1)}. ${lastName}`;
